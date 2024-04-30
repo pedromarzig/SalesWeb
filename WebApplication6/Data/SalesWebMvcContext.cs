8ee707d1ad00da84a0d4ae8e08a1a9b590e8f70f
Peddro <pedromarzigcastelani@gmail.com>
2024-04-30 08:03:51 -0300

    CRUD Scaffolding

---
+++ WebApplication6/Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Models;
+
+namespace WebApplication6.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication6.Models.Department> Department { get; set; }
+    }
+}
